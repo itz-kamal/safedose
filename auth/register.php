8d1a230b17ce32d22b4d40cc98296ad33fcd170a
itz-kamal <muphasa999@gmail.com>
2026-02-21 12:01:51 +0000

    Initial Commit

---
+++ auth/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Safe Dose - Register</title>
+</head>
+<body>
+  <div>
+    <div></div>
+    <footer>
+      <div>
+        <div></div>
+        <div>
+          <h4>Secure System Initialization</h4>
+          <p>By completing this setup, you establish root administrative level. This account will have full authority over inventory, user management, and security protocols for your pharmacy.</p>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </footer>
+  </div>
+</body>
+</html>
