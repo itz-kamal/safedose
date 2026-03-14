e72a90a7f9056b0f4b52585a2602a56cba4a9cd8
EmmyisEmmy <emmanueleziafar@gmail.com>
2026-03-14 04:03:25 -0500

    create staff

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,44 +1,20 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-  </head>
-  <body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../dashboard.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+  <title>UserDashboard</title>
+</head>
+<body>
 
-      <div class="container-fluid">
-          <div class="row">
-            <div class="col-2 col-sm-3 col-xl-2 bg-dark">
-              <nav class="navbar bg-dark border-bottom border-white" data-bs-theme="dark">
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                </div>
-              </nav>
+<?php include("../include/sidebar.php"); ?>
 
-              <nav class="nav flex-column">
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
-                <a class="nav-link" href="#">Link</a>
-                <a class="nav-link" href="#">Link</a>
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </nav>
-          </div>
-              <div class="container">
+<div class="greeting">
+    <h3>Latest Information</h3>
+</div>
 
-            </div>
-
-            <div class="col-10 col-sm-9 col-xl-10">
-              <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-md">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                </div>
-              </nav>
-            </div>
-          </div>
-      </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
---
+++ include/sidebar.php
@@ -0,0 +1,35 @@
+        <div class="navigation_bar">
+
+            <div class="logo">
+                <img src= "/SAFEDOSE/images/logo.svg" alt="SafeDose Logo" width="50">
+                <span>Safedose</span>
+            </div>
+            <ul class="nav-items">
+              <li class="first_item">
+                <a href="/SAFEDOSE/dashboard/index.php">
+                  <i class="fa-solid fa-house"></i>
+                  <span>Dashboard</span>
+                </a>
+
+              </li>
+              <li>
+                <a href="/SAFEDOSE/createstaff.php">
+                  <i class="fa-solid fa-user-plus"></i>
+                  <span>Create Staff</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa-solid fa-users"></i>
+                  <span>View Staff</span>
+                </a>
+              </li>
+
+            </ul>
+
+
+        </div>
+
+        <div class="greeting">
+            <h1>Hello, Admin</h1>
+        </div>
