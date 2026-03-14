69c462b7f9a005cbcbe75a23cf7ea5864fa4d5ec
c-mbama <c.mbama@rgu.ac.uk>
2026-03-14 12:31:40 +0000

    Done the staff creation

--- include/sidebar.php
+++ include/sidebar.php
@@ -19,7 +19,7 @@
       </a>
     </li>
     <li>
-      <a href="/safedose/dashboard/admin/staff-list.php">
+      <a href="#">
         <i class="fa-solid fa-users"></i>
         <span>View Staff</span>
       </a>
